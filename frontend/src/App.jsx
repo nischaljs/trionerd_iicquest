5aefdf6fbbebc583d69edc0e40779cbd0dad3f29
Rahultharu064 <rahultharu46882@icloud.com>
2025-06-11 18:27:05 +0545

    login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ChalkBoxDashboard from "./components/dashboard/ChalkBoxDashboard";
 import Dashboard from "./components/dashboard/Dashboard";
+import LoginForm from "./components/form/LoginForm";
+
 
 const App = () => {
   const router = createBrowserRouter([
@@ -10,6 +12,10 @@ const App = () => {
       path: "/",
       element: <HomePage />,
     },
+   {
+    path:"/login",
+    element:<LoginForm />
+   },
     {
       path: "/dashboard",
       element: <ChalkBoxDashboard />,
